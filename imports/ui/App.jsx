133cf2f91063bff7b3facddd84141013b3c21a9f
Sandro Queiroz Jr <sandroqz@gmail.com>
2017-10-30 05:38:05 -0300

    first commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { createContainer } from 'meteor/react-meteor-data'
+
+import { Tasks } from '../api/tasks'
+
+import Task from './Task'
+
+// App component - represents the whole app
+class App extends Component {
+  renderTasks() {
+    return this.props.tasks.map(task => <Task key={task._id} task={task} />)
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+
+        <ul>{this.renderTasks()}</ul>
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  tasks: PropTypes.array.isRequired
+}
+
+export default createContainer(() => {
+  return {
+    tasks: Tasks.find({}).fetch()
+  }
+}, App)
